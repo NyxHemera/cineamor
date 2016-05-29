0e4c5c0784152de90ab3788e04953e36d1586a9e
Jordan Rust <jordan.rust92@gmail.com>
2016-05-29 09:10:28 -0400

    Added additional routes and functionality, cleaned up old views.
    Deleted a good amount of .ejs views that were no longer useful. I need to also clean up the routes eventually.
    
    Added in the search functionality which shows when you press the addMovie button in the sidenav. This needs some cue to go away though.

--- public/javascripts/collections.controller.js
+++ public/javascripts/collections.controller.js
@@ -1,13 +1,77 @@
 angular.module('cineAmorApp')
-.controller('collectionsCtrl', function(collectionsPromise, collectionPromise, CollectionsService, $state) {
+.controller('collectionsCtrl', function(collectionsPromise, collectionPromise, CollectionsService, $state, $http) {
 	var vm = this;
 
 	vm.collection = collectionPromise.data;
 	vm.collections = collectionsPromise.data;// = CollectionsService.loadAllCollections();
 
-	console.log(vm.collections);
-	console.log(vm.collection);
+	vm.querySearch = querySearch;
+	vm.selectedItemChange = selectedItemChange;
+	vm.searchTextChange = searchTextChange;
+	//vm.cid = currentCollection;
+	vm.addMovieToCollection = addMovieToCollection;
+
 	vm.openCollection = function(collection) {
 		$state.go('collection', {collectionId: collection._id});
 	};
+
+	function searchTextChange(text) {
+		console.log('Text changed to ' + text);
+	}
+
+	function selectedItemChange(item) {
+		console.log('Item changed to ' + JSON.stringify(item));
+		if(item) {
+			loadMovieToPage(item);
+		}
+	}
+
+	function loadMovieToPage(movie) {
+		$http.get('http://www.omdbapi.com/?i='+ movie.imdbID)
+			.then(function(results) {
+				vm.itemShown = results.data;
+			});
+	}
+
+	function addMovieToCollection() {
+		console.log('AMTC called');
+		$http.put('/collections/'+vm.collection._id, vm.itemShown)
+			.then(function() {
+				console.log('Movie Saved');
+			});
+	}
+
+
+	// Search Functions
+	function querySearch(query) {
+		return $http.get('http://imdb.wemakesites.net/api/search?q=' + query)
+			.then(function(results) {
+				console.log(results);
+				if(results.data.data.results.titles) {
+					return processData(results.data.data.results.titles);
+				}else {
+					return [];
+				}
+			});
+/*		return $http.get('http://www.omdbapi.com/?s=' + query + '&type=movie')
+			.then(function(results) {
+				if(results.data.Search) {
+					return processData(results.data.Search);
+				}else {
+					return [];
+				}
+			});*/
+	}
+
+  function processData(data) {
+
+  	return data.map(function(movie) {
+  		return {
+  			value: movie.title.toLowerCase(),
+  			display: movie.title,
+  			Title: movie.title,
+  			imdbID: movie.id
+  		};
+  	});
+  }
 });
--- public/javascripts/main.controller.js
+++ public/javascripts/main.controller.js
@@ -1,6 +1,16 @@
 angular.module('cineAmorApp')
-.controller('mainCtrl', function($mdSidenav) {
+.controller('mainCtrl', function($mdSidenav, $stateParams, $state) {
 	var vm = this;
+
+	vm.msRevealed = false;
+
+	vm.isCollectionPage = function() {
+		return $state.current.name === 'collection';
+	}
+
+	vm.showMovieSearch = function() {
+		vm.msRevealed = !vm.msRevealed;
+	}
 
 	vm.openLeftMenu = function() {
 		$mdSidenav('left').toggle();
